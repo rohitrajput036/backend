638cce32c8303d097ad688bf9e1e20b5cb69bc6d
Mansi <mansi@gmail.com>
2022-10-11 18:28:09 +0530

    addmission file

---
+++ application/controllers/api/Admission.php
@@ -0,0 +1,68 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Admission extends REST_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('admission_model');
+    }
+
+    public function index_get() {
+        $StartTime = microtime(true);
+        $response = [
+            'Control' => [
+                'Status' => 0,
+                'Message' => 'You have wrong direction, Please check the manual',
+                'MessageCode' => REST_Controller::HTTP_BAD_REQUEST,
+                'TimeTaken' => (microtime(true) - $StartTime) . ' Second'
+            ],
+            'Data' => []
+        ];
+        $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    public function index_post() {
+        $StartTime = microtime(true);
+        $response = [
+            'Control' => [
+                'Status' => 0,
+                'Message' => 'You have wrong direction, Please check the manual',
+                'MessageCode' => REST_Controller::HTTP_BAD_REQUEST,
+                'TimeTaken' => (microtime(true) - $StartTime) . ' Second'
+            ],
+            'Data' => []
+        ];
+        $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    function add_post(){
+        $start_time = microtime(true);
+        try{
+            $request = json_decode($this->input->raw_input_stream);
+            $api_name = __CLASS__ . '/' . chop(__FUNCTION__, '_post');
+            $uuid = property_exists($request->control,"request_id") ? $request->control->request_id : generateUUId();
+            $this->log4php->log('info', 'REQUEST', $api_name, $uuid, $request, 0);
+            if (!empty($request)) {
+                keyExist(['control','data'],$request);
+                keyExist(['request_id','source','request_time'],$request->control);
+            };
+
+        }catch (Exception $E) {
+            $this->log4php->log('error', 'ERROR', $api_name, $uuid, $E->getMessage(), 0);
+            $Response = [
+                'control' => [
+                    'status' => 0,
+                    'message' => $E->getMessage(),
+                    'message_code' => $E->getCode(),
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->response($Response, $E->getCode());
+        }
+
+    }
+}
