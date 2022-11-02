f0f70f9cd343b17f73b5fe6bcc3bc9f68eb930b2
Mansi <mansi@gmail.com>
2022-11-02 18:38:54 +0530

    file

--- application/controllers/Routes.php
+++ application/controllers/Routes.php
@@ -12,6 +12,22 @@ class Routes extends CI_Controller {
     }
 
     public function index() {
+        $api_url = API_URL.'route_master/get';
+        $request = [
+            'control' => [
+                'request_id' => generateUUId(),
+                'source' => 1,
+                'request_time' => time(),
+                'version' => API_VERSION
+            ],
+            'data' =>  [
+                'branch_id' => userdata('BranchId'),
+                'is_active' => 1
+            ]
+        ];
+        $response = callAPI($api_url,'POST',json_encode($request));
+        $routes_info = (isset($response['data'])) ? $response['data'] : [];
+        $this->outputData['routes_info'] = $routes_info;
         $this->parser->parse("settings/routes.tpl", $this->outputData);
     }
 }
