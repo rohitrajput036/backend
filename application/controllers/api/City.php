395b5caa604357435efda2b31880c59ac48b0415
Mansi <mansi@gmail.com>
2022-11-04 18:06:03 +0530

    file

--- application/controllers/api/City.php
+++ application/controllers/api/City.php
@@ -1,40 +1,156 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
+class Area extends REST_Controller {
 
-class City extends REST_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model('city_model');
     }
 
     public function index_get() {
-        $start_time = microtime(true);
+        $StartTime = microtime(true);
         $response = [
-            'control' => [
-                'status' => 0,
-                'message' => 'You have wrong direction, Please check the manual',
-                'message_code' => REST_Controller::HTTP_BAD_REQUEST,
-                'time_taken' => (microtime(true) - $start_time) . ' Second'
+            'Control' => [
+                'Status' => 0,
+                'Message' => 'You have wrong direction, Please check the manual',
+                'MessageCode' => REST_Controller::HTTP_BAD_REQUEST,
+                'TimeTaken' => (microtime(true) - $StartTime) . ' Second'
             ],
-            'data' => []
+            'Data' => []
         ];
         $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
     }
 
     public function index_post() {
-        $start_time = microtime(true);
+        $StartTime = microtime(true);
         $response = [
-            'control' => [
-                'status' => 0,
-                'message' => 'You have wrong direction, Please check the manual',
-                'message_code' => REST_Controller::HTTP_BAD_REQUEST,
-                'time_taken' => (microtime(true) - $start_time) . ' Second'
+            'Control' => [
+                'Status' => 0,
+                'Message' => 'You have wrong direction, Please check the manual',
+                'MessageCode' => REST_Controller::HTTP_BAD_REQUEST,
+                'TimeTaken' => (microtime(true) - $StartTime) . ' Second'
             ],
-            'data' => []
+            'Data' => []
         ];
         $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    function add_post(){
+        $start_time = microtime(true);
+        try{
+            $request = json_decode($this->input->raw_input_stream);
+            $api_name = __CLASS__ . '/' . chop(__FUNCTION__, '_post');
+            $uuid = property_exists($request->control,"request_id") ? $request->control->request_id : generateUUId();
+            $this->log4php->log('info', 'REQUEST', $api_name, $uuid, $request, 0);
+            if (!empty($request)) {
+                keyExist(['control', 'data'],$request);
+                keyExist(['request_id', 'source', 'request_time'],$request->control);
+                keyExist(['city_id', 'state_id', 'city_name', 'sort_order'], $request->data);
+                checkBlank(['request_id' => $request->control->request_id,'source' => $request->control->source,'request_time' => $request->control->request_time]);
+                checkBlank(['state_id' => $request->data->state_id, 'city_name' => $request->data->city_name]);
+                $this->city_model->city_id = $request->data->city_id;
+                $this->city_model->state_id = $request->data->state_id;
+                $this->city_model->city_name = $request->data->city_name;
+                $this->city_model->sort_order = $request->data->sort_order;
+                $this->city_model->is_active = 1;
+                $this->city_model->created_by = $this->city_model->updated_by = (isset($request->data->login_id) && $request->data->login_id > 0) ? $request->data->login_id : 0;
+                $message = 'city Add successfully!';
+                if(isset($request->data->city_id) && $request->data->city_id > 0){
+                    $this->city_model->city_id = $request->data->city_id;
+                    $this->city_model->update();
+                    $message = 'city update successfully!';
+                }else{
+                    if(!$this->city_model->check()){
+                        $this->city_model->add();
+                    }else{
+                        throw new Exception('This city aready exists!',400);
+                    }
+                }
+            }else{
+                throw new Exception('Invalid request',400);
+            }
+            $response = [
+                'control' => [
+                    'status' => 1,
+                    'message' => $message,
+                    'message_code' => REST_Controller::HTTP_OK,
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, REST_Controller::HTTP_OK);
+
+        }catch (Exception $E) {
+            $this->log4php->log('error', 'ERROR', $api_name, $uuid, $E->getMessage(), 0);
+            $response = [
+                'control' => [
+                    'status' => 0,
+                    'message' => $E->getMessage(),
+                    'message_code' => $E->getCode(),
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, $E->getCode());
+        }
+    }
+
+    function delete_post(){
+        $start_time = microtime(true);
+        try {
+            $request = json_decode($this->input->raw_input_stream);
+            $api_name = __CLASS__ . '/' . chop(__FUNCTION__, '_post');
+            $uuid = property_exists($request->control,"request_id") ? $request->control->request_id : generateUUId();
+            $this->log4php->log('info', 'REQUEST', $api_name, $uuid, $request, 0);
+            if (!empty($request)) {
+                keyExist(['control','data'],$request);
+                keyExist(['request_id','source','request_time'],$request->control);
+                keyExist(['city_id','is_active'],$request->data);
+                checkBlank(['request_id' => $request->control->request_id,'source' => $request->control->source,'request_time' => $request->control->request_time]);
+                checkBlank(['city_id' => $request->data->city_id,'is_active' => $request->data->is_active]);
+                $this->city_model->city_id = $request->data->city_id;
+                $this->city_model->is_active = $request->data->is_active;
+                $this->city_model->created_by = $this->city_model->updated_by = (isset($request->data->login_id) && $request->data->login_id > 0) ? $request->data->login_id : 0; 
+                $this->city_model->delete();
+                $message = 'City update successfully';
+                if($request->data->is_active == 1){
+                    $message = 'City activate successfully';
+                }else if($request->data->is_active == 2){
+                    $message = 'City dactivate successfully';
+                }else if($request->data->is_active == 3){
+                    $message = 'City delete successfully';
+                }
+            }else{
+                throw new Exception('Invalid request',400);
+            }
+            $response = [
+                'control' => [
+                    'status' => 1,
+                    'message' => $message,
+                    'message_code' => REST_Controller::HTTP_OK,
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, REST_Controller::HTTP_OK);
+        }catch (Exception $E) {
+            $this->log4php->log('error', 'ERROR', $api_name, $uuid, $E->getMessage(), 0);
+            $response = [
+                'control' => [
+                    'status' => 0,
+                    'message' => $E->getMessage(),
+                    'message_code' => $E->getCode(),
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, $E->getCode());
+        }
     }
 
     function get_post(){
@@ -48,17 +164,24 @@ class City extends REST_Controller {
                 if(isset($request->data->is_active)){
                     $this->city_model->is_active = $request->data->is_active;
                 }
+                if(isset( $request->data->city_id)){
+                    $this->city_model->city_id = $request->data->city_id;
+                }
                 if(isset($request->data->state_id)){
                     $this->city_model->state_id = $request->data->state_id;
                 }
+                if(!isset($request->data->for_table)){
+                    $request->data->for_table = false;
+                }
+                $this->city_model->for_table = $request->data->for_table;
                 $data = $this->city_model->get();
             } else {
                 throw new Exception("Invalid Request", REST_Controller::HTTP_BAD_REQUEST);
             }
             $response = [
-                'Control' => [
+                'control' => [
                     'status' => 1,
-                    'message' => 'List of City',
+                    'message' => 'List of city',
                     'message_code' => REST_Controller::HTTP_OK,
                     'time_taken' => (microtime(true) - $start_time) . ' Second'
                 ],
@@ -68,17 +191,17 @@ class City extends REST_Controller {
             $this->response($response, REST_Controller::HTTP_OK);
         } catch (Exception $E) {
             $this->log4php->log('error', 'ERROR', $api_name, $uuid, $E->getMessage(), 0);
-            $Response = [
+            $response = [
                 'control' => [
                     'status' => 0,
                     'message' => $E->getMessage(),
                     'message_code' => $E->getCode(),
                     'time_taken' => (microtime(true) - $start_time) . ' Second'
                 ],
-                'data' => [
-                ]
+                'data' => []
             ];
-            $this->response($Response, $E->getCode());
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, $E->getCode());
         }
     }
 }
