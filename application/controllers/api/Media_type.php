07b2b0b617faa64ee371368be988b5efa84a109a
Mansi <mansi@gmail.com>
2022-12-03 12:58:40 +0530

    file

--- application/controllers/api/Media_type.php
+++ application/controllers/api/Media_type.php
@@ -149,4 +149,51 @@ class Media_type extends REST_Controller {
             $this->response($response, $E->getCode());
         }
     }
+
+    function get_post(){
+        $start_time = microtime(true);
+        try {
+            $request = json_decode($this->input->raw_input_stream);
+            $api_name = __CLASS__ . '/' . chop(__FUNCTION__, '_post');
+            $uuid = property_exists($request->control,"request_id") ? $request->control->request_id : generateUUId();
+            $this->log4php->log('info', 'REQUEST', $api_name, $uuid, $request, 0);
+            if (!empty($request)) {
+                if(isset($request->data->is_active)){
+                    $this->media_type_model->is_active = $request->data->is_active;
+                }
+                if(isset($request->data->for_table)){
+                    $request->data->for_table = true;
+                }else{
+                    $request->data->for_table = false;
+                }
+                $data = $this->media_type_model->get($request->data->for_table);
+            } else {
+                throw new Exception("Invalid Request", REST_Controller::HTTP_BAD_REQUEST);
+            }
+            $response = [
+                'control' => [
+                    'status' => 1,
+                    'message' => 'Here is a your all media List!',
+                    'message_code' => REST_Controller::HTTP_OK,
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => $data
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, REST_Controller::HTTP_OK);
+        } catch (Exception $E) {
+            $this->log4php->log('error', 'ERROR', $api_name, $uuid, $E->getMessage(), 0);
+            $response = [
+                'control' => [
+                    'status' => 0,
+                    'message' => $E->getMessage(),
+                    'message_code' => $E->getCode(),
+                    'time_taken' => (microtime(true) - $start_time) . ' Second'
+                ],
+                'data' => []
+            ];
+            $this->log4php->log('info', 'RESPONSE', $api_name, $uuid, $response, 0);
+            $this->response($response, $E->getCode());
+        }
+    }
 }
