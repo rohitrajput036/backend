fcd1554ef2e47bdb00081245d4292fe471699459
Mansi <mansi@gmail.com>
2022-12-03 17:28:04 +0530

    file

--- application/models/Media_type_model.php
+++ application/models/Media_type_model.php
@@ -52,7 +52,7 @@ class Media_type_model extends CI_Model {
         return $this->global_model->update($this->table_name,$update_data,$where);
     }
 
-    function get(){
+    function get($data_table = false){
         $output = [];
         if(!empty($this->is_active)){
             $where['is_active'] = $this->is_active;
@@ -64,15 +64,33 @@ class Media_type_model extends CI_Model {
         }
         $order_by = NULL;
         $results = $this->global_model->select($this->table_name,$where,'*',NULL,NULL,NULL,$order_by);
-        if(isset($results) && $results->num_rows() > 0){
-            foreach($results->result() as $result){
-                $output[] = [
-                    'media_type_id' => $result->media_type_id,
-                    'media_type'    => $result->media_type,
-                    'is_active'         => $result->is_active,
-                    'created_by'        => $result->created_by,
-                    'created_on'        => $result->created_on
-                ];
+        if(isset($resutls) && $resutls->num_rows() > 0){
+            $i=0;
+            foreach($resutls->result() as $result){
+                ++$i;
+                if($data_table){
+                    if($result->is_active == 1){
+                        $active_deactive_btn = '<btn class="btn active_deactive" data-media_type_id="'.$result->media_type_id.'" data-at="2"><i class="fa fa-check text-green"></i></btn>';
+                    }else{
+                        $active_deactive_btn = '<btn class="btn active_deactive" data-media_type_id="'.$result->media_type_id.'" data-at="1"><i class="fa fa-times text-red"></i></btn>';
+                    }
+                    $delete_btn = '<btn class="btn active_deactive" data-media_type_id="'.$result->media_type_id.'" data-at="3"><i class="fa fa-trash text-red"></i></btn>';
+                    $edit_btn = '<btn class="btn edit" data-media_type_id="'.$result->media_type_id.'"><i class="fa fa-pencil-square-o text-primary"></i></btn>';
+                    $btns = $active_deactive_btn. $delete_btn. $edit_btn;
+                    $output[] = [
+                        $i,
+                        '<span id="media_type'.$result->media_type_id.'">'.$result->media_type.'</span>',
+                        $btns
+                    ];
+                }else{
+                    $output[] = [
+                        'media_type_id' => $result->media_type_id,
+                        'media_type'    => $result->media_type,
+                        'is_active'     => $result->is_active,
+                        'created_by'    => $result->created_by,
+                        'created_on'    => $result->created_on
+                    ];
+                }
             }
         }
         return $output;
